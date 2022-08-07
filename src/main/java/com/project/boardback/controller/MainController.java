19ad88f1e01eb66044818b8ca02bbc569a67f8e1
leeyeonji28 <yeon102800@gmail.com>
2022-08-07 18:02:55 +0900

    Question, Answer Entity 작성

--- src/main/java/com/project/boardback/controller/MainController.java
+++ src/main/java/com/project/boardback/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.project.boardback;
+package com.project.boardback.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
