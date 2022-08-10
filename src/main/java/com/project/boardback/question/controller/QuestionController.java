5f348ce030acea995d758690b82eb1a10ab4f049
leeyeonji28 <yeon102800@gmail.com>
2022-08-10 22:48:43 +0900

    Question Detail

--- src/main/java/com/project/boardback/question/controller/QuestionController.java
+++ src/main/java/com/project/boardback/question/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.project.boardback.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,12 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/project/boardback/question/service/QuestionService.java
+++ src/main/java/com/project/boardback/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.project.boardback.question.service;
 
+import com.project.boardback.question.DataNotFoundException;
 import com.project.boardback.question.dao.QuestionRepository;
 import com.project.boardback.question.domain.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){ // id값으로 Question 데이터 조회하는 메소드
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()){ // 해당 데이터가 있는지 검사
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question no found");
+        }
     }
 }
