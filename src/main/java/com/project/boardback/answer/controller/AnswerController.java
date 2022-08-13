d43505c8b043f057a9b5b4f0aabd1410367372c6
leeyeonji28 <yeon102800@gmail.com>
2022-08-13 14:06:28 +0900

    answer create

---
+++ src/main/java/com/project/boardback/answer/controller/AnswerController.java
@@ -0,0 +1,27 @@
+package com.project.boardback.answer.controller;
+
+import com.project.boardback.answer.service.AnswerService;
+import com.project.boardback.question.domain.Question;
+import com.project.boardback.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
---
+++ src/main/java/com/project/boardback/answer/service/AnswerService.java
@@ -0,0 +1,25 @@
+package com.project.boardback.answer.service;
+
+import com.project.boardback.answer.dao.AnswerRepository;
+import com.project.boardback.answer.domain.Answer;
+import com.project.boardback.question.domain.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
